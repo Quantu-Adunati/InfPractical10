e6a6144ee002f6e4191865ddf2cecb9b580a36d8
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-04-25 12:08:58 +0200

    Fixed random range

--- prac.py
+++ prac.py
@@ -20,7 +20,7 @@ def countPrime(num):
     return len(n)
       
 def randomgen():
-    return random.randint(25,51)
+    return random.randint(25,50)
 
 def generateRandom(dividedNumber):
     alpha =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
